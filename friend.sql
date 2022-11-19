7d3c8c5a834797d2b8d8b827370c65aacd652189
lizarbeth <116977357+lizarbeth@users.noreply.github.com>
2022-11-18 22:09:49 -0600

    Update friend.sql

--- friend.sql
+++ friend.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2022 at 04:31 AM
+-- Generation Time: Nov 19, 2022 at 05:08 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -31,8 +31,18 @@ CREATE DATABASE friend;
 CREATE TABLE `friends` (
   `user1` varchar(50) NOT NULL,
   `user2` varchar(50) NOT NULL,
-  `friendship` tinyint(1) NOT NULL
+  `friendship` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `friends`
+--
+
+INSERT INTO `friends` (`user1`, `user2`, `friendship`) VALUES
+('blueberry21', 'hydrobro1995', 1),
+('lizarbeth', 'blueberry21', 1),
+('lizarbeth', 'hydrobro1995', 0),
+('sharkgirl', 'lizarbeth', 1);
 
 -- --------------------------------------------------------
 
@@ -53,7 +63,15 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`postText`, `user`, `date`, `time`, `postID`) VALUES
-('This is my first post.', 'lizarbeth', '2022-11-12', '09:08:45', 1);
+('This is my first post.', 'lizarbeth', '2022-11-12', '09:08:45', 1),
+('Status update: currently at work (for once)', 'blueberry21', '2022-11-16', '03:13:19', 2),
+('hello', 'lizarbeth', '2022-11-16', '10:37:04', 3),
+('I\'m available to chat :-)', 'lizarbeth', '2022-11-16', '10:39:16', 4),
+('I LOVE WATER!!!!!!!!', 'hydrobro1995', '2022-11-17', '12:48:25', 10),
+('I love posting on here', 'lizarbeth', '2022-11-17', '07:48:58', 11),
+('this is another test post :-)', 'lizarbeth', '2022-11-17', '07:54:24', 14),
+('this is another test post :-)', 'lizarbeth', '2022-11-17', '07:54:44', 15),
+('Hi, I\'m new here my name is Shark Girl', 'sharkgirl', '2022-11-17', '05:29:56', 16);
 
 -- --------------------------------------------------------
 
@@ -67,16 +85,19 @@ CREATE TABLE `users` (
   `lastName` varchar(50) NOT NULL,
   `password` varchar(150) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `count` int(20) NOT NULL
+  `count` int(20) NOT NULL,
+  `pic` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `firstName`, `lastName`, `password`, `email`, `count`) VALUES
-('lizarbeth', 'Elisabeth', 'McMichael', 'LiZarBeth123!!', 'c00432278@louisiana.edu', 1),
-('blueberry21', 'Lavender', 'Gooms', 'blackANDtan', 'lg21@gmail.com', 2);
+INSERT INTO `users` (`username`, `firstName`, `lastName`, `password`, `email`, `count`, `pic`) VALUES
+('lizarbeth', 'Elisabeth', 'McMichael', 'LiZarBeth123!!', 'c00432278@louisiana.edu', 1, 'pp1.jpg'),
+('blueberry21', 'Lavender', 'Gooms', 'blackANDtan', 'lg21@gmail.com', 2, ''),
+('hydrobro1995', 'Ruben', 'Gold', 'iLoveWater!!$', 'rubengold1995@yahoo.com', 3, 'pp2.jpg'),
+('sharkgirl', 'Janet', 'Planet', 'lavaBOY', 'shkgrl@gmail.com', 4, '');
 
 --
 -- Indexes for dumped tables
@@ -112,13 +133,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `postID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `postID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `count` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `count` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -141,4 +162,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
