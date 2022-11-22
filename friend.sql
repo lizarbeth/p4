e43bfc1aae1b71f66e2f3e0936666aeef921e531
lizarbeth <116977357+lizarbeth@users.noreply.github.com>
2022-11-22 12:59:47 -0600

    Update friend.sql

--- friend.sql
+++ friend.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2022 at 05:08 AM
+-- Generation Time: Nov 22, 2022 at 07:58 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -26,24 +26,47 @@ CREATE DATABASE friend;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `postID` int(50) NOT NULL,
+  `commenter` varchar(50) NOT NULL,
+  `commentText` text NOT NULL,
+  `commentID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `friends`
 --
 
 CREATE TABLE `friends` (
   `user1` varchar(50) NOT NULL,
-  `user2` varchar(50) NOT NULL,
-  `friendship` tinyint(1) NOT NULL DEFAULT 0
+  `user2` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `friends`
 --
 
-INSERT INTO `friends` (`user1`, `user2`, `friendship`) VALUES
-('blueberry21', 'hydrobro1995', 1),
-('lizarbeth', 'blueberry21', 1),
-('lizarbeth', 'hydrobro1995', 0),
-('sharkgirl', 'lizarbeth', 1);
+INSERT INTO `friends` (`user1`, `user2`) VALUES
+('blueberry21', 'hydrobro1995'),
+('lizarbeth', 'blueberry21'),
+('sharkgirl', 'lizarbeth');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `postID` int(50) NOT NULL,
+  `liker` varchar(100) NOT NULL,
+  `likeID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -56,23 +79,24 @@ CREATE TABLE `posts` (
   `user` varchar(50) NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL,
-  `postID` int(50) NOT NULL
+  `postID` int(50) NOT NULL,
+  `likeCount` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`postText`, `user`, `date`, `time`, `postID`) VALUES
-('This is my first post.', 'lizarbeth', '2022-11-12', '09:08:45', 1),
-('Status update: currently at work (for once)', 'blueberry21', '2022-11-16', '03:13:19', 2),
-('hello', 'lizarbeth', '2022-11-16', '10:37:04', 3),
-('I\'m available to chat :-)', 'lizarbeth', '2022-11-16', '10:39:16', 4),
-('I LOVE WATER!!!!!!!!', 'hydrobro1995', '2022-11-17', '12:48:25', 10),
-('I love posting on here', 'lizarbeth', '2022-11-17', '07:48:58', 11),
-('this is another test post :-)', 'lizarbeth', '2022-11-17', '07:54:24', 14),
-('this is another test post :-)', 'lizarbeth', '2022-11-17', '07:54:44', 15),
-('Hi, I\'m new here my name is Shark Girl', 'sharkgirl', '2022-11-17', '05:29:56', 16);
+INSERT INTO `posts` (`postText`, `user`, `date`, `time`, `postID`, `likeCount`) VALUES
+('This is my first post.', 'lizarbeth', '2022-11-12', '09:08:45', 1, 2),
+('Status update: currently at work (for once)', 'blueberry21', '2022-11-16', '03:13:19', 2, 1),
+('hello', 'lizarbeth', '2022-11-16', '10:37:04', 3, 0),
+('I\'m available to chat :-)', 'lizarbeth', '2022-11-16', '10:39:16', 4, 0),
+('I LOVE WATER!!!!!!!!', 'hydrobro1995', '2022-11-17', '12:48:25', 10, 3),
+('I love posting on here', 'lizarbeth', '2022-11-17', '07:48:58', 11, 1),
+('this is another test post :-)', 'lizarbeth', '2022-11-17', '07:54:24', 14, 0),
+('this is another test post :-)', 'lizarbeth', '2022-11-17', '07:54:44', 15, 0),
+('Hi, I\'m new here my name is Shark Girl', 'sharkgirl', '2022-11-17', '05:29:56', 16, 2);
 
 -- --------------------------------------------------------
 
@@ -95,14 +119,22 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `firstName`, `lastName`, `password`, `email`, `count`, `pic`) VALUES
-('lizarbeth', 'Elisabeth', 'McMichael', 'LiZarBeth123!!', 'c00432278@louisiana.edu', 1, 'pp1.jpg'),
-('blueberry21', 'Lavender', 'Gooms', 'blackANDtan', 'lg21@gmail.com', 2, ''),
-('hydrobro1995', 'Ruben', 'Gold', 'iLoveWater!!$', 'rubengold1995@yahoo.com', 3, 'pp2.jpg'),
-('sharkgirl', 'Janet', 'Planet', 'lavaBOY', 'shkgrl@gmail.com', 4, '');
+('lizarbeth', 'Elisabeth', 'McMichael', 'LiZarBeth123!!', 'c00432278@louisiana.edu', 1, 'pp2.jpg'),
+('blueberry21', 'Lavender', 'Gooms', 'blackANDtan', 'lg21@gmail.com', 2, 'pp4.jpg'),
+('hydrobro1995', 'Ruben', 'Gold', 'iLoveWater!!$', 'rubengold1995@yahoo.com', 3, 'pp1.jpg'),
+('sharkgirl', 'Janet', 'Planet', 'lavaBOY', 'shkgrl@gmail.com', 4, 'pp3.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`commentID`),
+  ADD KEY `postID` (`postID`),
+  ADD KEY `commenter` (`commenter`);
 
 --
 -- Indexes for table `friends`
@@ -110,6 +142,14 @@ INSERT INTO `users` (`username`, `firstName`, `lastName`, `password`, `email`, `
 ALTER TABLE `friends`
   ADD PRIMARY KEY (`user1`,`user2`),
   ADD KEY `FK2` (`user2`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`likeID`),
+  ADD KEY `fk3` (`liker`),
+  ADD KEY `postID` (`postID`);
 
 --
 -- Indexes for table `posts`
@@ -131,6 +171,18 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `commentID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `likeID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
@@ -147,11 +199,25 @@ ALTER TABLE `users`
 --
 
 --
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`postID`) REFERENCES `posts` (`postID`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`commenter`) REFERENCES `users` (`username`);
+
+--
 -- Constraints for table `friends`
 --
 ALTER TABLE `friends`
   ADD CONSTRAINT `FK1` FOREIGN KEY (`user1`) REFERENCES `users` (`username`),
   ADD CONSTRAINT `FK2` FOREIGN KEY (`user2`) REFERENCES `users` (`username`);
+
+--
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `fk3` FOREIGN KEY (`liker`) REFERENCES `users` (`username`),
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`postID`) REFERENCES `posts` (`postID`);
 
 --
 -- Constraints for table `posts`
