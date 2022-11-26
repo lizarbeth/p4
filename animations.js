94eed2f2ab79929186221b57dda12d4734c984f6
OwynB <116918758+OwynB@users.noreply.github.com>
2022-11-26 12:40:39 -0600

    Update animations.js
    fixed the JavaScript for animating the icons on each post

--- animations.js
+++ animations.js
@@ -1,11 +1,5 @@
 window.onload = function () {
 
-    document.getElementById("replyiconlisten").addEventListener("click",replyFunction)
-
-    document.getElementById("bottleiconlisten").addEventListener("click",bottleiconfunction)
-
-    document.getElementById("dropleticonlisten").addEventListener("click",dropleticonfunction)
-
     var gear = document.getElementById("gear");
     gear.addEventListener("mouseover",gearfunction)
     gear.addEventListener("mouseout",gearfunctionout)
@@ -21,14 +15,32 @@ window.onload = function () {
     var home = document.getElementById("home");
     home.addEventListener("mouseover",homefunction)
     home.addEventListener("mouseout",homefunctionout)
-    
+
     /* js for comment button */
     var add = document.getElementsByClassName("showComments");
     add.onclick = showForm;
     var form = document.getElementsByClassName("display");
 
-}
 
+    /* js for animating the icons on each post */
+    var reply = document.getElementsByClassName("fa-solid fa-reply replyiconin");
+    for (let i = 0; i < reply.length; i++) {
+        reply[i].addEventListener("click", iconfunction);
+    }
+
+    var bottle = document.getElementsByClassName("fa-solid fa-bottle-water bottleiconin");
+    for (let i = 0; i < bottle.length; i++) {
+        bottle[i].addEventListener("click", iconfunction);
+    }
+
+    var droplet = document.getElementsByClassName("fa-solid fa-droplet dropleticonin");
+    for (let i = 0; i < droplet.length; i++) {
+        droplet[i].addEventListener("click", iconfunction);
+    }
+
+
+}
+/* js for sidebar animated icons */
 function gearfunction() {
     var x = document.getElementById("gearin");
     x.classList.add("fa-spin");
@@ -81,26 +93,14 @@ function homefunctionout() {
     y.classList.remove("card");
 }
 
-function replyFunction() {
-    var x = document.getElementById("replyiconin");
-    x.classList.toggle("colorone");
-    x.classList.toggle("colortwo");
-    x.classList.toggle("fa-beat");
+/* js function for animating icon on each post */
+function iconfunction() {
+    this.classList.toggle("colorone");
+    this.classList.toggle("colortwo");
+    this.classList.toggle("fa-beat");
 }
-function bottleiconfunction() {
-    var x = document.getElementById("bottleiconin");
 
-    x.classList.toggle("colortwo")
-    x.classList.toggle("fa-beat");
-    x.classList.toggle("colorone");
 
-}
-function dropleticonfunction() {
-    var x = document.getElementById("dropleticonin");
-    x.classList.toggle("fa-beat");
-    x.classList.toggle("colorone");
-    x.classList.toggle("colortwo");
-}
 /* js for show comments */
 function showForm(formID){
     var form = document.getElementById(formID);
@@ -110,3 +110,4 @@ function showForm(formID){
         form.style.display = "none";
     }
 }
+
