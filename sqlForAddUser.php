ac2bf06393df0f922ab6b3650c1214bb55dd532a
lizarbeth <116977357+lizarbeth@users.noreply.github.com>
2022-11-27 22:12:02 -0600

    Update sqlForAddUser.php

--- sqlForAddUser.php
+++ sqlForAddUser.php
@@ -1,40 +1,37 @@
 <?php
-
+	date_default_timezone_set('America/Chicago');
 	$database = new mysqli("localhost", "INFX371", "P*ssword", "friend");
 	
 	$user = trim($_POST['user']);
 	$password = trim($_POST['password']);
-	$confirmPassword = trim($_POST['confirmPassword']);
 	$fName = trim($_POST['FName']);
 	$lName = trim($_POST['LName']);
 	$email = trim($_POST['email']);
 	$email2 = trim($_POST['email2']);
-
 	
+	$joined = date('Y-m-d');
+
 	$num = rand(1, 4);
 	$picture = "pp{$num}.jpg";
 	
-	if ($password != $confirmPassword){
-		header( 'Location: AddUser.php?msg3' );
-	}
+
 	
-	elseif ($email != $email2){
+	if ($email != $email2){
 		header( 'Location: AddUser.php?msg2' );
 	}
 	else{
 		$hashed_password = password_hash($password, PASSWORD_DEFAULT); //password_hash adds salt by default
 	
 	
-		$sql = "INSERT INTO users (username, password, firstName, lastName, email, pic) 
-				VALUES ('$user', '$hashed_password', '$fName', '$lName', '$email', '$picture')";
+		$sql = "INSERT INTO users (username, password, firstName, lastName, email, pic, joined) 
+				VALUES ('$user', '$hashed_password', '$fName', '$lName', '$email', '$picture','$joined')";
 		if(!$database->query($sql)){
 			header( 'Location: AddUser.php?msg' );
 		}
 		else{
-			header( 'Location: login.php' );
+			header( 'Location: AddUser.php' );
 		}
 	}
-	
 	
 
 ?>
