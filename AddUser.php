2a6e39d1088a3463c3606043c66552c10982ec2e
lizarbeth <116977357+lizarbeth@users.noreply.github.com>
2022-11-28 21:02:22 -0600

    Update AddUser.php

--- AddUser.php
+++ AddUser.php
@@ -10,6 +10,7 @@
     <script src="https://kit.fontawesome.com/c924802615.js" crossorigin="anonymous"></script>
     <script src="animations.js"></script>
     <title>Watering Hole Sign Up</title>
+	<link rel="icon" href="smallicon.png">
 </head>
 
 <title>AddUser</title>
